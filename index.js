d4f4b8e4bfd87931f231505b30576c20762e3518
FreshyBot <54530562+FreshyBot@users.noreply.github.com>
2019-08-26 11:56:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const botconfig = require("./botconfig.json");
+const token = require("./token.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
